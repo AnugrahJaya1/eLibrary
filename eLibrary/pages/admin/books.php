932cf9625c3ca7b72bceccb3bcff8b1b88517911
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-05-10 09:51:49 +0700

    sudah bisa add buku dan search

--- eLibrary/pages/admin/books.php
+++ eLibrary/pages/admin/books.php
@@ -1,37 +1,85 @@
 <?php
-if(!isset($_SESSION))
-{
-    session_start();
-}
-include("../../connection/connection.php");
-$sql="SELECT *,NamaKategori FROM
-        buku INNER JOIN kategori_buku ON buku.CodeBuku=kategori_buku.CodeBuku
-        INNER JOIN kategori ON kategori.CodeKategori=kategori_buku.CodeKategori";
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+    include("../../connection/connection.php");
+    $sql="SELECT *,NamaKategori FROM
+            buku INNER JOIN kategori_buku ON buku.CodeBuku=kategori_buku.CodeBuku
+            INNER JOIN kategori ON kategori.CodeKategori=kategori_buku.CodeKategori";
 
-if(isset($_POST["searchButt"])){
-    $search=$_POST["bookSearch"];
-    $select=$_POST["select"];
+    if(isset($_POST["searchButt"])){
+        $search=$_POST["bookSearch"];
+        $select=$_POST["select"];
 
-    if(isset($search) && $search!=""){
-        if($select=="Tittle"){
-            $sql .=" WHERE Tittle LIKE '%$search%' ORDER BY Tittle ASC";
-        }else if($select=="Author"){
-            $sql .=" WHERE Author LIKE '%$search%' ORDER BY Author ASC";
-        }else if($select=="Publish Year"){
-            $sql .=" WHERE Publication Year LIKE '%$search%' ORDER BY Publis Year ASC";
-        }else if($select=="Publisher"){
-            $sql .=" WHERE Publisher LIKE '%$search%' ORDER BY Publisher ASC";
-        }else if($select=="Category"){
-            $sql .=" WHERE Kategori LIKE '%$search%' ORDER BY Kategori ASC";
+        if(isset($search) && $search!=""){
+            if($select=="Title"){
+                $sql .=" WHERE Tittle LIKE '%$search%' ORDER BY Tittle ASC";
+            }else if($select=="Author"){
+                $sql .=" WHERE Author LIKE '%$search%' ORDER BY Author ASC";
+            }else if($select=="Publish Year"){
+                $sql .=" WHERE Publication Year LIKE '%$search%' ORDER BY Publis Year ASC";
+            }else if($select=="Publisher"){
+                $sql .=" WHERE Publisher LIKE '%$search%' ORDER BY Publisher ASC";
+            }else if($select=="Category"){
+                $sql .=" WHERE Kategori LIKE '%$search%' ORDER BY Kategori ASC";
+            }
+        }
+
+    }
+    if(isset($_POST["iAdd"])){
+        $title=$_POST["title"];
+        $author=$_POST["author"];
+        $pusblish_year=$_POST["publishyear"];
+        $publish=$_POST["publish"];
+        $kategori=$_POST["category"];
+        $stock=$_POST["stock"];
+
+        if(isset($title) && isset($author) && isset($pusblish_year) && isset($publish) && isset($kategori) && isset($stock)){
+            if($title!="" && $author!="" && $pusblish_year!="" && $publish!="" && $kategori!="" && $stock!=""){
+                $codeBuku = generateCode(4);
+                $sql="SELECT CodeBuku FROM buku WHERE CodeBuku='$codeBuku'";
+                $res=$mysqli->query($sql);
+                //handling jika ada duplica code
+                if($res->num_rows!=0){
+                    while($res->num_rows!=0){
+                        $codeBuku=generateCode(4);
+                        $$sql="SELECT CokeBuku FROM buku WHERE CodeBuku='$codeBuku'";
+                        $res=$mysqli->query($sql);
+                    }
+                }
+                //insert to buku
+                $sql="INSERT INTO `buku`(`CodeBuku`, `Tittle`, `Author`, `Publication Year`, `Publisher`, `Stock`) 
+                    VALUES ('$codeBuku','$title','$author','$pusblish_year','$publish','$stock')";
+                $mysqli->query($sql);
+
+                $kategori=$kategori;
+                $sql="SELECT `CodeKategori` FROM `kategori` WHERE LIKE NamaKategori='$kategori'";
+                if($res=$mysqli->query($sql)){
+                    while($row=$res->fetch_array()){
+                        
+                        $codeKategori=$row['CodeKategori'];
+                        // echo $codeKategori;
+                        //insert kategori buku
+                        $sql="INSERT INTO `kategori_buku`(`CodeBuku`, `CodeKategori`) VALUES ('$codeBuku','$codeKategori')";
+                        $res=$mysqli->query($sql);
+                    }
+                }
+                
+            }
         }
     }
-    
-    
-    // $sql="SELECT *,NamaKategori FROM
-    // buku INNER JOIN kategori_buku ON buku.CodeBuku=kategori_buku.CodeBuku
-    // INNER JOIN kategori ON kategori.CodeKategori=kategori_buku.CodeBuku";
 
-}
+    function generateCode($length){
+        // $l=$length;
+        $char="0123456789ABCDEFGHIJKLMNOPQRSTUPWXYZ";
+        $charLength=strlen($char);
+        $randomCode="";
+        for($i=0;$i<$length;$i++){
+            $randomCode .=$char[rand(0,$charLength-1)];
+        }
+        return $randomCode;
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -89,22 +137,23 @@ if(isset($_POST["searchButt"])){
                             <th>Stock</th>
                         </tr><?php
                 // echo $sql;
-                if($res=$mysqli->query($sql)){
-                while($row=$res->fetch_array()){
-                echo "<tr>";
-                echo "<td>".$row["CodeBuku"]."</td>";
-                echo "<td>".$row["Tittle"]."</td>";
-                echo "<td>".$row["Author"]."</td>";
-                echo "<td>".$row["Publication Year"]."</td>";
-                echo "<td>".$row["Publisher"]."</td>";
-                echo "<td>".$row["NamaKategori"]."</td>";
-                echo "<td>".$row["Stock"]."</td>";
-                echo "</tr>";
-                }
-                }
                 $sql="SELECT *,NamaKategori FROM
                 buku INNER JOIN kategori_buku ON buku.CodeBuku=kategori_buku.CodeBuku
                 INNER JOIN kategori ON kategori.CodeKategori=kategori_buku.CodeKategori";
+                if($res=$mysqli->query($sql)){
+                    while($row=$res->fetch_array()){
+                    echo "<tr>";
+                    echo "<td>".$row["CodeBuku"]."</td>";
+                    echo "<td>".$row["Tittle"]."</td>";
+                    echo "<td>".$row["Author"]."</td>";
+                    echo "<td>".$row["Publication Year"]."</td>";
+                    echo "<td>".$row["Publisher"]."</td>";
+                    echo "<td>".$row["NamaKategori"]."</td>";
+                    echo "<td>".$row["Stock"]."</td>";
+                    echo "</tr>";
+                    }
+                }
+                
                 ?>
                     </table>
                 </div>
@@ -124,17 +173,25 @@ if(isset($_POST["searchButt"])){
                             <br />
                             <input class="modalForm" type="text" placeholder="Author" name="author" />
                             <br />
-                            <input class="modalForm" type="text" placeholder="Publish Year" name="publishyear" />
+                            <input class="modalForm" type="text" pattern="^[ ]*[+]?[0-9]{4,}[ ]*$" placeholder="Publish Year" name="publishyear" />
                             <br />
                             <input class="modalForm" type="text" placeholder="Publisher" name="publish" />
                             <br />
+                            <input class="modalForm" type="text" pattern="^[ ]*[+]?[0-9]{1,}[ ]*$" placeholder="Stock" name="stock" />
+                            <br>
                             <select class="modalForm" name="category">
-                                <option value="Novel">Novel</option>
-                                <option value="Encyclopedia">Encyclopedia</option>
-                                <option value="Magazine">Magazine</option>
+                                <?php 
+                                    $sql="SELECT NamaKategori FROM kategori";
+                                    if($res=$mysqli->query($sql)){
+                                        while($row=$res->fetch_array()){
+                                            $namaKategori=$row['NamaKategori'];
+                                            echo "<option value='$namaKategori'>".$namaKategori."</option>";
+                                        }
+                                    }
+                                ?>
                             </select>
                             <br />
-                            <input class="modalForm" type="button" id="upBott" value="ADD">
+                            <input class="modalForm" type="submit" id="upBott" name="iAdd" value="ADD">
                         </form>
 
                     </div>
