beceabd9c08eca7dcaaebff51c45ae51cc7bbf3f
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-05-10 22:01:19 +0700

    Add files via upload

--- eLibrary/pages/user/book.php
+++ eLibrary/pages/user/book.php
@@ -84,22 +84,23 @@ if(!isset($_SESSION))
                         </tr>
                         <?php 
                             // echo $sql;
-                            if($res=$mysqli->query($sql)){
-                                while($row=$res->fetch_array()){
-                                    echo "<tr>";
-                                        echo "<td>".$row["CodeBuku"]."</td>";
-                                        echo "<td>".$row["Tittle"]."</td>";
-                                        echo "<td>".$row["Author"]."</td>";
-                                        echo "<td>".$row["Publication Year"]."</td>";
-                                        echo "<td>".$row["Publisher"]."</td>";
-                                        echo "<td>".$row["NamaKategori"]."</td>";
-                                        echo "<td>".$row["Stock"]."</td>";
-                                    echo "</tr>";
-                                }
-                            }
-                            $sql="SELECT *,NamaKategori FROM
-                                    buku INNER JOIN kategori_buku ON buku.CodeBuku=kategori_buku.CodeBuku
-                                    INNER JOIN kategori ON kategori.CodeKategori=kategori_buku.CodeKategori";
+                            include("../../controller/getBookList.php");
+                            // if($res=$mysqli->query($sql)){
+                            //     while($row=$res->fetch_array()){
+                            //         echo "<tr>";
+                            //             echo "<td>".$row["CodeBuku"]."</td>";
+                            //             echo "<td>".$row["Tittle"]."</td>";
+                            //             echo "<td>".$row["Author"]."</td>";
+                            //             echo "<td>".$row["Publication Year"]."</td>";
+                            //             echo "<td>".$row["Publisher"]."</td>";
+                            //             echo "<td>".$row["NamaKategori"]."</td>";
+                            //             echo "<td>".$row["Stock"]."</td>";
+                            //         echo "</tr>";
+                            //     }
+                            // }
+                            // $sql="SELECT *,NamaKategori FROM
+                            //         buku INNER JOIN kategori_buku ON buku.CodeBuku=kategori_buku.CodeBuku
+                            //         INNER JOIN kategori ON kategori.CodeKategori=kategori_buku.CodeKategori";
 
                         ?>
                     </table>
