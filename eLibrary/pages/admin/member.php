eb11ddc0b9bacaa959dd9fd63d52bd3c1c68beb9
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-05-10 11:43:16 +0700

    Add files via upload
    udah bisa nampilin member dan search member

--- eLibrary/pages/admin/member.php
+++ eLibrary/pages/admin/member.php
@@ -1,8 +1,25 @@
 <?php
-if(!isset($_SESSION))
-{
-    session_start();
-}
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    include("../../connection/connection.php");
+    $sql="SELECT IdAnggota,Nama,Phone,Alamat FROM anggota WHERE IsAdmin='0'";
+    
+    if(isset($_POST['searchButt'])){
+        $search=$_POST['memberSearch'];
+        $select=$_POST['select'];
+
+        if($select=='ID'){
+            $sql .=" AND  IdAnggota LIKE '%$search%'";
+        }else if($select=='Name'){
+            $sql .=" AND  Nama LIKE '%$search%'";
+        }else if($select=='Phone'){
+            $sql .=" AND  Phone LIKE '%$search%'";
+        }else{
+            $sql .=" AND  Alamat LIKE '%$search%'";
+        }
+    }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,11 +43,11 @@ if(!isset($_SESSION))
             <div id="title">
                 <h1>Book List</h1>
                 <form id="searchBar" name="searchBar" method="post" action="">
-                    <input type="search" name="bookSearch" id="search" placeholder="Search member.." />
+                    <input type="search" name="memberSearch" id="search" placeholder="Search member.." />
                     by
                     <select name="select">
                         <option value="ID">ID</option>
-                        <option value="Name">Author</option>
+                        <option value="Name">Name</option>
                         <option value="Phone">Phone</option>
                         <option value="Address">Address</option>
                     </select>
@@ -45,7 +62,20 @@ if(!isset($_SESSION))
                             <th>Phone</th>
                             <th>Address</th>
                         </tr>
-
+                        <?php
+                            // echo $sql;
+                            if($res=$mysqli->query($sql)){
+                                while($row=$res->fetch_array()){
+                                    echo "<tr>";
+                                        echo "<td>".$row['IdAnggota']."</td>";
+                                        echo "<td>".$row['Nama']."</td>";
+                                        echo "<td>".$row['Phone']."</td>";
+                                        echo "<td>".$row['Alamat']."</td>";
+                                    echo "</tr>";
+                                }
+                            }
+                            $sql="SELECT IdAnggota,Nama,Phone,Alamat FROM anggota WHERE IsAdmin='0'";
+                        ?>
                     </table>
                 </div>
             </div>
