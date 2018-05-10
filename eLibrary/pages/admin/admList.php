e676d07125cb9a6d0de50a4d33d6434f92b40be7
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-05-10 18:21:35 +0700

    Add files via upload
    sudah bisa add dan search

--- eLibrary/pages/admin/admList.php
+++ eLibrary/pages/admin/admList.php
@@ -1,8 +1,64 @@
 <?php
-if(!isset($_SESSION))
-{
-    session_start();
-}
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+    include("../../connection/connection.php");
+    $sql="SELECT IdAnggota,Nama,Phone,Alamat FROM anggota WHERE IsAdmin='1'";
+
+    // handle search
+    if(isset($_POST['searchButt'])){
+        $search=$_POST['adminSearch'];
+        $select=$_POST['select'];
+
+        if(isset($search) && $search!=""){
+            if($select=="ID"){
+                $sql .=" AND IdAnggota LIKE '%$search%'";
+            }else if($select=="Name"){
+                $sql .=" AND Nama LIKE '%$search%'";
+            }else if($select=="Phone"){
+                $sql .=" AND Phone LIKE '%$search%'";
+            }else{
+                $sql .=" AND AlamatLIKE '%$search%'";
+            }
+        }
+    }
+
+    // handle add Admin
+    $statusAddAdmin=false;
+    $message="";
+    if(isset($_POST['iAdd'])){
+        $username=$_POST['username'];
+        $nama=$_POST['nama'];
+        $phone=$_POST['phone'];
+        $address=$_POST['address'];
+
+        $sql1="SELECT username FROM anggota WHERE username='$username'";
+        if($res=$mysqli->query($sql1)){
+            if($res->num_rows ==0){
+                $password=generatePass(8);
+                $sql1="INSERT INTO anggota (username,Password,Nama,Phone,Alamat,IsAdmin) VALUES ('$username','$password','$nama','$phone','$address','1')";
+                $mysqli->query($sql1);
+                $statusAddAdmin=true;
+                $message="Administrator add,temporary password : $password. Please change it immediately";
+            }else{
+                $message="Username not available, please try again";
+                $statusAddAdmin=false;
+            }
+            // echo $message;
+        }
+    }
+
+    function generatePass($length){
+        $char="0123456789abcdefghijklmnopqrstupwxyzABCDEFGHIJKLMNOPQRSTUPWXYZ";
+        $charLength=strlen($char);
+        $randomPass="";
+        for($i=0;$i<$length;$i++){
+            $randomPass .=$char[rand(0,$charLength-1)];
+        }
+        return $randomPass;
+    }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,11 +84,11 @@ if(!isset($_SESSION))
                 <h1>Admin List</h1>
                 <div id="formCont">
                     <form id="searchBar" name="searchBar" method="post" action="">
-                        <input type="search" name="bookSearch" id="search" placeholder="Search admin.." />
+                        <input type="search" name="adminSearch" id="search" placeholder="Search admin.." />
                         by
                         <select name="select">
                             <option value="ID">ID</option>
-                            <option value="Name">Author</option>
+                            <option value="Name">Name</option>
                             <option value="Phone">Phone</option>
                             <option value="Address">Address</option>
                         </select>
@@ -41,7 +97,15 @@ if(!isset($_SESSION))
                     </form>
                     <button id="modalButt" onclick="openModal()">ADD ADMIN</button>
                 </div>
+                
             </div>
+
+            <?php
+                if(($statusAddAdmin==false || $statusAddAdmin==true) && $message!=""){
+                    echo $message;
+                }
+            ?>
+
             <!--ADMIN TABLE-->
             <div id="tableCont">
                 <table cellspacing="0">
@@ -51,7 +115,19 @@ if(!isset($_SESSION))
                         <th>Phone</th>
                         <th>Address</th>
                     </tr>
-
+                    <?php 
+                        // $sql="SELECT IdAnggota,Nama,Phone,Alamat FROM anggota WHERE IsAdmin='1'";
+                        if($res=$mysqli->query($sql)){
+                            while($row=$res->fetch_array()){
+                                echo "<tr>";
+                                    echo "<td>".$row['IdAnggota']."</td>";
+                                    echo "<td>".$row['Nama']."</td>";
+                                    echo "<td>".$row['Phone']."</td>";
+                                    echo "<td>".$row['Alamat']."</td>";
+                                echo "</tr>";
+                            }
+                        }
+                    ?>
                 </table>
             </div>
         </div>
@@ -74,7 +150,7 @@ if(!isset($_SESSION))
                         <br />
                         <input class="modalForm" type="text" placeholder="Adress" name="address" />
                         <br />
-                        <input class="modalForm" type="button" id="upBott" value="ADD">
+                        <input class="modalForm" type="submit" name="iAdd" id="upBott" value="ADD">
                     </form>
 
                 </div>
