b16229f876fc3a34dbc5525fbbb5cb4cb1be0bb8
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-05-10 22:01:02 +0700

    Add files via upload

--- eLibrary/pages/admin/books.php
+++ eLibrary/pages/admin/books.php
@@ -163,26 +163,10 @@
                             <th>Publisher</th>
                             <th>Category</th>
                             <th>Stock</th>
-                        </tr><?php
-                // echo $sql;
-                
-                if($res=$mysqli->query($sql)){
-                    while($row=$res->fetch_array()){
-                    echo "<tr>";
-                    echo "<td>".$row["CodeBuku"]."</td>";
-                    echo "<td>".$row["Tittle"]."</td>";
-                    echo "<td>".$row["Author"]."</td>";
-                    echo "<td>".$row["Publication Year"]."</td>";
-                    echo "<td>".$row["Publisher"]."</td>";
-                    echo "<td>".$row["NamaKategori"]."</td>";
-                    echo "<td>".$row["Stock"]."</td>";
-                    echo "</tr>";
-                    }
-                }
-                // $sql="SELECT *,NamaKategori FROM
-                // buku INNER JOIN kategori_buku ON buku.CodeBuku=kategori_buku.CodeBuku
-                // INNER JOIN kategori ON kategori.CodeKategori=kategori_buku.CodeKategori";
-                ?>
+                        </tr>
+                        <?php 
+                            include("../../controller/getBookList.php");
+                        ?>
                     </table>
                 </div>
             </div>
